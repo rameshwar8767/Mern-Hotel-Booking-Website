72b40bd88ea5368117a8c2cc0bec9f025fc50b60
rameshwar mane <manerameshwar909@gmail.com>
2025-06-25 11:58:25 +0530

    Add MyBookings page component for managing hotel reservations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
+import MyBookings from './pages/MyBookings';
 
 const App = () => {
   const location = useLocation();
@@ -17,6 +18,7 @@ const App = () => {
           <Route path="/" element={<HomePage/>} />
           <Route path="/rooms" element={<AllRooms/>} />
           <Route path="/rooms/:id" element={<RoomDetails/>} />
+          <Route path="/my-bookings" element={<MyBookings/>} />
         </Routes>
 
       </div>
