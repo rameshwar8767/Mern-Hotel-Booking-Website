2b7e0ab620c4891ebef0771a4c06a8d8504b58e1
rameshwar mane <manerameshwar909@gmail.com>
2025-06-26 16:35:20 +0530

    Add hotel owner features: implement layout, navbar, sidebar, and pages for dashboard, add room, and list room

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom'
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 const App = () => {
   const location = useLocation();
@@ -13,12 +18,18 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path="/" element={<HomePage/>} />
           <Route path="/rooms" element={<AllRooms/>} />
           <Route path="/rooms/:id" element={<RoomDetails/>} />
           <Route path="/my-bookings" element={<MyBookings/>} />
+          <Route path='/owner' element={<Layout/>}>
+            <Route index element={<Dashboard/>} />
+            <Route path='add-room' element={<AddRoom/>} />
+            <Route path='list-room' element={<ListRoom/>} />
+          </Route>
         </Routes>
 
       </div>
